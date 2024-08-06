6b8bc8af163ebdb56877d2b540351216e4fe47a7
Nacho Vanzo <9426@holbertonstudents.com>
2024-08-06 16:47:05 +0000

    Calcular niveles hacia arriba - Ex10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ *medir - Calcula la cantidad de niveles de un nodo hacia arriba
+ *Return: retorna la longitud
+ *@tree: puntero al nodo
+ */
+
+size_t medir(const binary_tree_t *tree)
+{
+	size_t a;
+
+	if (tree == NULL)
+		return (0);
+
+
+	a = 1 + medir(tree->parent);
+
+		return (a);
+}
+
+/**
+ *binary_tree_height - Calcular cantidad de niveles de un nodo
+ *Return: retorna la longitud
+ *@tree: puntero al nodo para calcular cantidad de niveles hacia arriba
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (medir(tree) - 1);
+}
