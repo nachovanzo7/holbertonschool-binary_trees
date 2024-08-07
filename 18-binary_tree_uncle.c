78abbe909e4c1bdfeb80cc9f1379bb6607f62b59
Nacho Vanzo <9426@holbertonstudents.com>
2024-08-07 18:02:24 +0000

    Betty Fix Ex-18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,13 +16,15 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent == NULL)
 		return (NULL);
 	if (node->parent->parent == NULL)
-	  return (NULL);
-	
-	if (node->parent->parent->right == node->parent && node->parent->parent->left != NULL)
-		return (node->parent->parent->left);
+		return (NULL);
 
-	else if (node->parent->parent->left == node->parent && node->parent->parent->left != NULL)
-		return (node->parent->parent->right);
+	if (node->parent->parent->right == node->parent)
+		if (node->parent->parent->left != NULL)
+			return (node->parent->parent->left);
+
+	else if (node->parent->parent->left == node->parent)
+		if (node->parent->parent->left != NULL)
+			return (node->parent->parent->right);
 
 	return (NULL);
 }
