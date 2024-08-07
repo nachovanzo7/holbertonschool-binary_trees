226d126e3dbc62b65036b45e0a8dda208785fb9d
ariel <ariel@gmail.com>
2024-08-07 17:43:12 +0000

    a

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ *binary_tree_uncle - Devolver hermano de node
+ *Return: puntero al nodo hermano
+ *@node: puntero a nodo
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+	  return (NULL);
+	
+	if (node->parent->parent->right == node->parent && node->parent->parent->left != NULL)
+		return (node->parent->parent->left);
+
+	else if (node->parent->parent->left == node->parent && node->parent->parent->left != NULL)
+		return (node->parent->parent->right);
+
+	return (NULL);
+}
